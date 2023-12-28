3a87e368f1a72a34c1f21e29386f7be74ddf39d5
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2023-12-28 13:13:30 +0100

    afterSave triggers: Log object id

--- src/triggers.js
+++ src/triggers.js
@@ -391,9 +391,9 @@ function logTriggerAfterHook(triggerType, className, input, auth, logLevel) {
   }
   const cleanInput = logger.truncateLogMessage(JSON.stringify(input));
   logger[logLevel](
-    `${triggerType} triggered for ${className} for user ${userIdForLog(
-      auth
-    )}:\n  Input: ${cleanInput}`,
+    `${triggerType} triggered for ${className} for user ${userIdForLog(auth)} on object ${
+      input.objectId
+    }:\n  Input: ${cleanInput}`,
     {
       className,
       triggerType,
@@ -409,9 +409,9 @@ function logTriggerSuccessBeforeHook(triggerType, className, input, result, auth
   const cleanInput = logger.truncateLogMessage(JSON.stringify(input));
   const cleanResult = logger.truncateLogMessage(JSON.stringify(result));
   logger[logLevel](
-    `${triggerType} triggered for ${className} for user ${userIdForLog(
-      auth
-    )}:\n  Input: ${cleanInput}\n  Result: ${cleanResult}`,
+    `${triggerType} triggered for ${className} for user ${userIdForLog(auth)} on object ${
+      input.objectId ?? '<no id>'
+    }:\n  Input: ${cleanInput}\n  Result: ${cleanResult}`,
     {
       className,
       triggerType,
@@ -426,9 +426,9 @@ function logTriggerErrorBeforeHook(triggerType, className, input, auth, error, l
   }
   const cleanInput = logger.truncateLogMessage(JSON.stringify(input));
   logger[logLevel](
-    `${triggerType} failed for ${className} for user ${userIdForLog(
-      auth
-    )}:\n  Input: ${cleanInput}\n  Error: ${JSON.stringify(error)}`,
+    `${triggerType} failed for ${className} for user ${userIdForLog(auth)} on object ${
+      input.objectId ?? '<no id>'
+    }:\n  Input: ${cleanInput}\n  Error: ${JSON.stringify(error)}`,
     {
       className,
       triggerType,
